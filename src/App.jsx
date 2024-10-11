a4c33d307a906f7b4d6902bd3efa52cbfe90ed83
Vishnu_1029 <vishnuvardhanreddy1029@gmail.com>
2024-10-11 11:24:58 +0530

    Routing Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './Pages/HomePage/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { HomePage } from './Pages/HomePage/HomePage';
 
 const App = () => {
- 
   return (
+  <BrowserRouter>
+    <div className="App">
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+};
 
-     <>
-     <div className="App">
-      <HomePage/>
-     </div>
-      
-    </>
-    
-  )
-}
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import {Provider} from "react-redux";
-import store from "./redux/store"
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
-  <Provider store = {store}>
+  <Provider store={store}>
+   <BrowserRouter>
     <App />
-  </Provider>,
-)
+     </BrowserRouter>
+  </Provider>
+);
